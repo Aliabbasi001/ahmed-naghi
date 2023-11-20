ad33fce08be38cbdfbef81d3793baebb43249ce6
aliabbasi <ali.abs.ir1@gmail.com>
2023-11-20 12:32:01 +0330

    add project to new git

---
+++ section3/slider/js/main.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
